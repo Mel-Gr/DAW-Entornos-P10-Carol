5b29f628ed11d08c3da2c54e9c55fcff072560e2
Oscar <dev@debian>
2023-03-17 19:18:37 +0100

    metodo de repetir x veces dos string.

--- src/main/java/ieseuropa/Hilo.java
+++ src/main/java/ieseuropa/Hilo.java
@@ -1,10 +1,25 @@
 package main.java.ieseuropa;
 
+import java.util.Scanner;
+
 public class Hilo {
+
+	public static String devolverXVeces(String uno, String dos, int veces) {
+
+		String resultado = "";
+
+		for (int i = 0; i < veces; i++) {
+			resultado += uno + " " + dos + " ";
+		}
+
+		return resultado;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+
+		System.out.println(devolverXVeces("uno" , "dos" , 2));
 	}
 
 }
