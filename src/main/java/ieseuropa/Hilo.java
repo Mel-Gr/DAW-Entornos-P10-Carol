72a4209c94006a0cf204e0698926c39e329bdbc6
Oscar <dev@debian>
2023-03-21 02:55:16 +0100

    metodos para cambiar de mayusculas a minuscula y viceversa y para saber el tamaño del string intruducido

--- src/main/java/ieseuropa/Hilo.java
+++ src/main/java/ieseuropa/Hilo.java
@@ -1,10 +1,15 @@
 package main.java.ieseuropa;
 
+import java.util.Scanner;
+
 public class Hilo {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+
+		
 	}
 
 }
