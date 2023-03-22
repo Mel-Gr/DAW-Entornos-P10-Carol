e59a8e6a74a4a8cb9445851df23df453c5e82de1
OscarDH <chusmita.oscar@gmail.com>
2023-03-22 18:46:48 +0100

    duplicarLetras

--- src/main/java/ieseuropa/Hilo.java
+++ src/main/java/ieseuropa/Hilo.java
@@ -10,16 +10,26 @@ public class Hilo {
 
 	public static String minus(String string) {
 		return string = string.toLowerCase();
-  }
-  
+	}
+
 	public static int longitud(String string) {
 		int size = string.length();
 		return size;
 	}
 
+	private static String duplicarLetras(String string) {
+		String resultado = "";
+		for (int i = 0; i < string.length(); i++) {
+			resultado += string.charAt(i);
+			resultado += string.charAt(i);
+		}
+		return resultado;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+		duplicarLetras("NBA");
 	}
 
 }
