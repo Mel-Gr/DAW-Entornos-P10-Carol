97137e36f98328930eed5dcc7065a5d12d98d2aa
dev <dev@debian>
2023-03-23 04:52:55 +0100

    modificacion del metodo  para que se pueda meter numeros de mas de 3 digitos

--- src/main/java/ieseuropa/Numero.java
+++ src/main/java/ieseuropa/Numero.java
@@ -6,17 +6,19 @@ public class Numero {
 	public static boolean esNumeroArmstrong(int numero) {
 	    int suma = 0;
 	    int numeroOriginal = numero;
+	    int numDigitos = String.valueOf(numero).length();
 	    while (numero != 0) {
 	        int digito = numero % 10;
-	        suma += Math.pow(digito, 3);
+	        suma += Math.pow(digito, numDigitos);
 	        numero /= 10;
 	    }
 	    if (suma == numeroOriginal) {
 	    	return true;
-	    }else {
+	    } else {
 	    	return false;
 	    }
 	}
+
 
 
 
